784514aef0d4328df36ef78cb65b1087b9d6a159
Joao Marcos Aires Duarte <T912598@prodam.local>
2022-06-08 11:58:43 -0400

    Integração com o API em NodeJS por AJAX

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Criar uma Lista Telefônica em AngularJS
+### Dependências:
+    ### $ npm install express
+    ### $ npm install body-parser
+
+### Start as dependências dos Projeto: $ npm install
+    
---
+++ backend/contatosBackend.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// ------------------------------------
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+var contatos = [
+	{nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
+	{nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
+	{nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}},
+  {nome: "Pedro", telefone: "9999-8888", data: new Date(), operadora: {nome: "GVT", codigo: 25, categoria: "Fixo"}},
+  {nome: "Clara", telefone: "9999-7777", data: new Date(), operadora: {nome: "Embratel", codigo: 21, categoria: "Fixo"}}
+];
+var operadoras = [
+	{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
+	{nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
+	{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
+	{nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1},
+	{nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2}
+];
+
+app.listen(process.env.PORT || 3412);
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/contatos', function(req, res) {
+  res.json(contatos);
+});
+
+app.post('/contatos', function(req, res) {
+  contatos.push(req.body);
+  res.json(true);
+});
+
+app.get('/operadoras', function(req, res) {
+  res.json(operadoras);
+});
